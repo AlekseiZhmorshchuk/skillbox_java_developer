6bb9a4c0d1a50986f1de82ba2e890ba964610681
ZERO <zhmorshuk94@mail.ru>
2025-09-02 22:57:10 +0500

    Save

---
+++ Module5.1CollectionsArray/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+            //Задание номер 3.
+            final int ONE = 7;
+            final int TWO = 7;
+            String[][] x = new String[ONE][TWO];
+            for (int i = 0; i < ONE; i++) {
+                for (int j = 0; j < TWO; j++) {
+                    if (i == j) {
+                        x[i][j] = "X";
+                    } else if (i == (TWO - 1 - j)) {
+                        x[i][j] = "X";
+                    } else {
+                        x[i][j] = " ";
+                    }
+                    System.out.print(x[i][j]);
+                }
+                System.out.println();
+            }
+    }
+}
